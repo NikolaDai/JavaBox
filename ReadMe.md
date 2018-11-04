effc33615d148d8711f7b9493089656a29a6a675
Daniel Dai <335860828@qq.com>
2018-11-04 22:50:07 +0800

    add some study content about primitive types.

--- ReadMe.md
+++ ReadMe.md
@@ -1,5 +1,5 @@
 # JAVABOX
-##Lambda Expression 
+## Lambda Expression 
 早就听说JAVA 8中的Lambda表达式，但总是感觉工作中并不是必需品之一，因此未曾花点时间看看。近来，看一本Spring MVC的书，作者开篇又在强调这么一句话：“如果你不采用Lambda来简化代码，真是一件遗憾的事情！”正好刚完成一个项目，所以想静下心看看到底Lambda能否帮我简化项目中的若干代码。如果认为看官方文档更靠谱，请移步https://docs.oracle.com/javase/tutorial/java/javaOO/lambdaexpressions.html。谈及Lambda表达式，对匿名类要有些使用体会，从而对
 Lambda表达式带来的便利会有更深刻的体验。在你的代码中用过匿名类吗？了解匿名类吗？有的话，请移步Section 2。
 
--- src/DataType/PrimitiveType.java
+++ src/DataType/PrimitiveType.java
@@ -1,7 +1,16 @@
 package DataType;
 
 public class PrimitiveType {
+    static private byte aByteValue;
+    private short aShortValue;
+    private int aIntValue;
+    private long aLongValue;
+    private float aFloatValue;
+    private double aDoubleValue;
+    private boolean aBooleanValue;
+
     public static void main(String[] args){
         System.out.println(2.0-1.1);
+        aByteValue = 125;
     }
 }
